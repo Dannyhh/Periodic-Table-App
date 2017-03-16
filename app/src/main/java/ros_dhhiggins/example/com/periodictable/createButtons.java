9e05b82e8f395f74e4b8b0d87ca4027a1b83696b
ros_dhhiggins <dobbykiko@gmail.com>
2017-03-16 09:34:38 -0700

    Added Array for button creation.

--- app/src/main/java/ros_dhhiggins/example/com/periodictable/createButtons.java
+++ app/src/main/java/ros_dhhiggins/example/com/periodictable/createButtons.java
@@ -1,6 +1,10 @@
 package ros_dhhiggins.example.com.periodictable;
 
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by ros_dhhiggins on 3/16/2017.
@@ -9,6 +13,28 @@ import android.widget.GridLayout;
 public class createButtons{
 
         public void build(GridLayout grid){
+            ImageButton[] elementButtons = new ImageButton[126];
+            //126 is the number of cells in the main grid
+                for(int i=1; i<=126; i++){
+                    if((i>2 && i<18) ||
+                       (i>20 && i<31) ||
+                       (i>38 && i<49)){
+                        elementButtons[i] =null;
+                    }
+                   else {
+                    //elementButtons[i] = create a image button with the data on it;
+                    //elementButtons[i] = new ImageButton();
+
+
+
+
+                                    //create buttons with onclick that takes the button number
+                                    //sets button number as extra
+                                    //starts new activity with that extra and use String Array
+                                    //use extra to find the info we need about the element and display
+
+
+                }
 
         }
 }
