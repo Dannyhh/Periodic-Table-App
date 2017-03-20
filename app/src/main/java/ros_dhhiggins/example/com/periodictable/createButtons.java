86211a549b0767f6403560a4ce6db78a01eaca93
ros_dhhiggins <dobbykiko@gmail.com>
2017-03-20 09:30:49 -0700

    Buttons now can have an image associated from drawable resource file.

--- app/src/main/java/ros_dhhiggins/example/com/periodictable/createButtons.java
+++ app/src/main/java/ros_dhhiggins/example/com/periodictable/createButtons.java
@@ -1,40 +1,51 @@
 package ros_dhhiggins.example.com.periodictable;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.lang.reflect.Array;
+
+import static android.R.attr.type;
 
 /**
  * Created by ros_dhhiggins on 3/16/2017.
  */
 
 public class createButtons{
-
+    private Context context;
+        createButtons(Context context){
+            this.context = context.getApplicationContext();
+        }
         public void build(GridLayout grid){
             ImageButton[] elementButtons = new ImageButton[126];
-            //126 is the number of cells in the main grid
                 for(int i=1; i<=126; i++){
+
+                    String name; //the name of the images
+                    name = "image"+i;
+
                     if((i>2 && i<18) ||
                        (i>20 && i<31) ||
                        (i>38 && i<49)){
-                        elementButtons[i] =null;
+                        elementButtons[i] = null;
                     }
                    else {
-                    //elementButtons[i] = create a image button with the data on it;
-                    //elementButtons[i] = new ImageButton();
-
-
-
-
+                        elementButtons[i] = new ImageButton(context);
+                        elementButtons[i].setImageResource(getImage(context, name));
+                        // creates the imageButton and sets it with the image specified by name
+                    }
                                     //create buttons with onclick that takes the button number
                                     //sets button number as extra
                                     //starts new activity with that extra and use String Array
                                     //use extra to find the info we need about the element and display
-
-
                 }
 
         }
+                    private static int getImage(Context context, String name) {
+                        int resID = context.getResources().getIdentifier(name, "drawable", context.getPackageName());
+                        return resID;
+    }
 }
